303713e860b3de9158da857cdb145a8898cf4464
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-06-06 15:37:05 -0600

    We have added an input form

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ function Home() {
     return (
       <div>
        <h1>Home page</h1>
-      
+       <form>
+           <input type='text' placeholder="Username / Email"></input>
+           <input type='text' placeholder="Add post here."></input>
+           <input type="submit"></input>
+       </form>
       </div>
     );
   }
