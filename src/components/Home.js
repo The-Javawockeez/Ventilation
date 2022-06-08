006f2ebcab7ce5b58105031920c0bca0d994e913
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-06-08 15:20:10 -0600

    Now we can add post to our data and display it

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,40 @@
 import { useEffect, useState } from 'react'
-import { useParams, useNavigate } from "react-router-dom";
-
 function Home() {
   const [posts, setPosts] = useState([])
-  const [addPost, setAddPost] = useState({ artistName: "", albumName: "" })
+  const [addPost, setAddPost] = useState({ userName: "", title: "", content: "" })
 
-
-  // const params = useParams();
-  // const navigate = useNavigate();
-  // console.log(params)
 
   // // add new post and display--------------------------------------------------------------
-  // const handleChange = (event) => {
-  //   // console.log(event.target.value)
-  //   setAddPost({ ...addPost, [event.target.name]: event.target.value })
-  // }
+  const handleChange = (event) => {
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault()
-  //   fetch("http://localhost:4000/vinyls", {
-  //     method: 'POST',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify(addPost)
-  //   })
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       const copy = [...posts]
-  //       // replace vinyl with different data from the new database
-  //       copy.push(data.vinyl)
+    setAddPost({ ...addPost, [event.target.name]: event.target.value })
+  }
 
-  //       setPosts(copy)
-  //       setAddPost({ artistName: "", albumName: "" })
-  //       // console.log(addPost)
-  //       console.log(addPost)
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    fetch("https://infinite-brook-21883.herokuapp.com/Ventilation-api/user"
+      , {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(addPost)
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        // console.log('data is '+ data)
+        const copy = [...posts]
+        console.log("data is " + data.post)
+        copy.push(data.post)
 
-  //     })
-  // };
-  //------------------------------------------------------------------------
+        setPosts(copy)
+        setAddPost({ userName: "", title: "", content: "" })
+      })
+  };
+
+  // ------------------------------------------------------------------------
   // const handleRemove = () => {
   //   fetch("http://localhost:3000/vinyls/" + posts._id, {
   //     method: 'DELETE',
@@ -55,37 +50,45 @@ function Home() {
       .then(response => response.json())
       .then(data => setPosts(data.data))
   }, [])
-
-  // console.log(posts.vinyls)
-  const displayPosts = posts.map((post, index) => (
-    // console.log(post.userPosts[0].title)
-    <div key={index}>
-      <h3>{post.userName}</h3>
-        <p>{post.userPosts[0].title}</p> 
-        <p>{post.userPosts[0].content}</p> 
-      {/* <button 
+  // console.log('post')
+  // console.log(posts)
+  const display = posts.map((post, index) => {
+    // console.log('post is '+ post)
+   if(post) {
+  
+    return (
+      <div key={index}>
+        <h3>{post.userName}</h3>
+        <p>{post.title}</p>
+        <p>{post.content}</p>
+        {/* <button 
       onClick={handleRemove} 
-      >Remove post</button>  */}
-    </div>
-  ))
+      >Remove post</button>   */}
+      </div>
+    )
+   }
+  })
+
+
   //---------------------------------------------------------------------
 
 
 
-
+  // console.log("addPost is ", addPost)
   return (
     <div>
       <h1>Home page</h1>
-       {/* <form
-         onSubmit={handleSubmit}
-      > 
-         <input type='text' placeholder="Username / Email" name="artistName"
-         onChange={handleChange}
-          value={addPost.artistName}></input> 
-         <input type='text' placeholder="Add post here." name='albumName' onChange={handleChange} value={addPost.albumName}></input>
-        <input type="submit"></input> 
-      </form>  */}
-       {displayPosts}
+      <form
+        onSubmit={handleSubmit}
+      >
+        <input type='text' placeholder="Username" name="userName"
+          onChange={handleChange}
+          value={addPost.userName}></input>
+        <input type='text' placeholder="Title" name='title' onChange={handleChange} value={addPost.title}></input>
+        <input type='text' placeholder="Add post here" name='content' onChange={handleChange} value={addPost.content}></input>
+        <input type="submit"></input>
+      </form>
+      {display}
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+
+function User() {
+    return (
+      <div>
+       <h1>User page</h1>
+      </div>
+    );
+  }
+  
+  export default User;
+  
