e20e60c0ca84c1003598ce21120a4b99b80ce75b
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-06-08 18:59:10 -0400

    setup created a life advice component. put a form in the life advice component and did a fetch request insidec of a use effect

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import {Route, Routes, Link} from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
-import User from './components/User';
+import LifeAdvice from './components/LifeAdvice';
 function App() {
   return (
     <div className="App">
       <Link to='/' >Home</Link>
       <Link to='/about'>About</Link>
-      <Link to='/user'>User</Link>
+      <Link to='/life-advice'>Life Advice</Link>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
-        <Route path='/user' element={<User />}/>
+        <Route path='/life-advice' element={<LifeAdvice />}/>
       </Routes>
     </div>
   );
